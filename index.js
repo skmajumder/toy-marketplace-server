971c7abeb53415d1a7607327047da51a0e5b5338
skmajumder <skmajumder.cse@gmail.com>
2023-05-21 07:09:20 +0600

    stop await client connection

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const database = client.db("toysStoreDB");
     const toysCollection = database.collection("toys");
